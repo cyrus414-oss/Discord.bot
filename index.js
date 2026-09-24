7be1d96d742c984108220a453cb3fb79e4f5e2c4
cyrus414-oss <cyruuss.61@gmail.com>
2026-09-24 23:24:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('CraftingNW Bot Aktif! 🚀');
+    res.send('Bot aktif!');
 });
 
 app.listen(port, () => {
     console.log(`Web sunucusu ${port} portunda çalışıyor.`);
 });
 
-const { Client, GatewayIntentBits } = require('discord.js');
-
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
     ]
 });
 
-// Sistem modüllerini çağırıyoruz
-require('./karsilayici.js')(client);
-require('./tick.js')(client);
-require('./cekilis.js')(client); 
-require('./oylama.js')(client);
-require('./yanitlar.js')(client);
-require('./kufurvbengel.js')(client);
-
 client.once('ready', () => {
-    console.log(`Bot aktif: ${client.user.tag}`);
+    console.log(`Bot giriş yaptı: ${client.user.tag}`);
 });
 
-// Token'ı gizli tutmak için Render Environment değişkeninden alıyoruz
+// Tüm modüllerini buraya bağlıyoruz
+require('./kufurvbengel.js')(client);
+require('./cekilis.js')(client);
+require('./karsilayici.js')(client);
+require('./oylama.js')(client);
+require('./tick.js')(client);
+require('./yanitlar.js')(client);
+
 client.login(process.env.TOKEN);
