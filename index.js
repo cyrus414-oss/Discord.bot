aac5fc42b954a2d8bbd921e8d8b96951f21a7c67
cyrus414-oss <cyruuss.61@gmail.com>
2026-09-24 23:13:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,24 +21,17 @@ const client = new Client({
     ]
 });
 
-// Karşılama/çıkış sistemini çağırıyoruz
+// Sistem modüllerini çağırıyoruz
 require('./karsilayici.js')(client);
-
-// Ticket sistemini (tick.js) çağırıyoruz
 require('./tick.js')(client);
-
-// Çekiliş sistemini dahil et
 require('./cekilis.js')(client); 
-
 require('./oylama.js')(client);
-
 require('./yanitlar.js')(client);
-
 require('./kufurvbengel.js')(client);
 
 client.once('ready', () => {
     console.log(`Bot aktif: ${client.user.tag}`);
 });
 
-// Botunun Token'ı
-client.login('MTU1MjQxMjYzMzE0MTg3ODg0NQ.G5hx6G.ZyEaxOSK1fErI4jbX0iFlhtLJvb4aV8UT3eY-A');
+// Token'ı gizli tutmak için Render Environment değişkeninden alıyoruz
+client.login(process.env.TOKEN);
